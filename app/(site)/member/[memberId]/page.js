cd64f26ce041400358f31a3ae64c83e33d163b85
Diego <diegofreeman78@gmail.com>
2023-09-15 21:21:41 +0100

    date

--- app/(site)/member/[memberId]/page.js
+++ app/(site)/member/[memberId]/page.js
@@ -6,6 +6,7 @@ import { fetchInvestor, fetchUserMember } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { format } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 
 const MemberPage = async () => {
   const { id } = await currentUser();
@@ -14,7 +15,8 @@ const MemberPage = async () => {
   }
   const isMember = await fetchUserMember(id);
   const isInvestor = await fetchInvestor(id);
-  const date = format(isMember?.dob, 'PPP');
+  const zonedTime = utcToZonedTime(isMember?.dob, 'Africa/Lagos');
+  const date = format(zonedTime, 'dd/MM/yyyy');
 
   if (!isMember.isOnboarded && !isInvestor.isOnboarded) {
     return redirect('/accountType');
