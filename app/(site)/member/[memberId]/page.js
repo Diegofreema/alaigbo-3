09f69085342651dd4dfd0dd7ca9cb79dcbcdaf72
Diego <diegofreeman78@gmail.com>
2023-09-15 20:33:52 +0100

    moment

--- app/(site)/member/[memberId]/page.js
+++ app/(site)/member/[memberId]/page.js
@@ -2,7 +2,7 @@ import EventCard from '@/components/Events/EventCard';
 import IdentityCard from '@/components/IdentityCard';
 import MemberSidebar from '@/components/MemberSidebar';
 import { fetchInvestor, fetchUserMember } from '@/lib/actions/user.actions';
-
+import moment from 'moment';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
