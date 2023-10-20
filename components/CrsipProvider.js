ab8827b0ad6ebe7d7e244915913c2ea8f2cebad1
Diego <diegofreeman78@gmail.com>
2023-10-20 15:24:19 +0100

    ui updated

--- components/CrsipProvider.js
+++ components/CrsipProvider.js
@@ -7,7 +7,7 @@ export const CrispProvider = () => {
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
-  });
+  }, []);
   if (!mounted) {
     return null;
   }
