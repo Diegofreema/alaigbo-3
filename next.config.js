99853ded37397b181ff66a44e6b0f42288acfaf4
Diegofreema <104385367+Diegofreema@users.noreply.github.com>
2024-01-20 13:58:31 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,24 @@ const nextConfig = {
     serverComponentsExternalPackages: ['prisma'],
   },
   images: {
-    domains: [
-      'lh3.googleusercontent.com',
-      'uploadthing.com',
-      'utfs.io',
-      'img.clerk.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
     ],
   },
   webpack(config) {
