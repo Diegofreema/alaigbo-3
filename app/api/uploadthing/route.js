91b4935ff4ae02b933eaf14d3c76ab51ecca1bb6
Diegofreema <104385367+Diegofreema@users.noreply.github.com>
2024-01-30 10:13:06 +0100

    Update route.js

--- app/api/uploadthing/route.js
+++ app/api/uploadthing/route.js
@@ -5,4 +5,10 @@ import { ourFileRouter } from './core';
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
+  config: {
+    logLevel: 'error',
+    uploadthingSecret: process.env.UPLOADTHING_SECRET,
+    uploadthingId: process.env.UPLOADTHING_APP_ID,
+  },
 });
+
